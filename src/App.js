d7499452288d3de7c17202399f159e89926b3ea8
takshch <takshch089@gmail.com>
2022-09-03 19:46:03 +0530

    Adds /logs page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import LogsRoute from './routes/Logs';
 import MainRoute from './routes/Main';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainRoute />}></Route>
+        <Route path="/logs" element={<LogsRoute />}></Route>
       </Routes>
     </BrowserRouter>
   );
